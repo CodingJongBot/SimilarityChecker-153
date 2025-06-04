4125d57b977c21db8cb0956b813e81a92e9811b0
jongwon <oasis9311@gmail.com>
2025-06-04 16:25:36 +0900

    [refactoring] get max lenth score

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -2,9 +2,7 @@ public class SimilarityChecker {
     public double getSimilarity(String inputStr1, String inputStr2) {
         if(inputStr1 == null || inputStr2 == null) throw new IllegalArgumentException();
 
-        if(inputStr1.equals("ASD") && inputStr2.equals("DSA")){
-            return 60;
-        }
+        if(inputStr1.length() == inputStr2.length()) return 60;
 
         return 0;
     }
