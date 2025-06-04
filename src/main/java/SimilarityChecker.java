e36f8464c08aafc0f6c8207bb6590561533e8c99
jongwon <oasis9311@gmail.com>
2025-06-04 16:30:03 +0900

    [refactoring] get zero lenth score

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,9 +1,17 @@
 public class SimilarityChecker {
     public double getSimilarity(String inputStr1, String inputStr2) {
-        if(inputStr1 == null || inputStr2 == null) throw new IllegalArgumentException();
+        if (inputStr1 == null || inputStr2 == null) throw new IllegalArgumentException();
+        if (isSameLength(inputStr1, inputStr2)) return 60;
+        if (isDoubleLength(inputStr1, inputStr2)) return 0;
+        return -1;
+    }
 
-        if(inputStr1.length() == inputStr2.length()) return 60;
+    private boolean isDoubleLength(String inputStr1, String inputStr2) {
+        return inputStr1.length() >= 2 * inputStr2.length()
+                || inputStr2.length() >= 2 * inputStr1.length();
+    }
 
-        return 0;
+    private boolean isSameLength(String inputStr1, String inputStr2) {
+        return inputStr1.length() == inputStr2.length();
     }
 }
